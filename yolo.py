a19e00a845220e31e138a1d944f04cc340facdae
Sreenath Swaminathan <43269126+Sree0211@users.noreply.github.com>
2023-06-29 23:27:51 +0200

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -110,5 +110,5 @@ if len(idxs) > 0:
 			0.5, color, 2)
 # show the output image
 #cv2.imshow("Image", image)
-cv2.imwrite("output/Horses.jpg",image)
+cv2.imwrite("output/Required_name.jpg",image)
 cv2.waitKey(0)
